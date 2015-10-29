260579c370e18e196e47060bcc366e09e0e41c1c
Darren Duncan <darren@DarrenDuncan.net>
2015-10-28 20:57:53 -0700

    add initial Visual Studio 2013 solution with library+console+test projects, all with their default / template content and settings

---
+++ src/Muldis.D.Ref_Eng/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muldis.D.Ref_Eng
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/Muldis.D.Ref_Eng.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Muldis.D.Ref_Eng.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
