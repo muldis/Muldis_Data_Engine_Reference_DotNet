f45a4e5671dd17457e5df98a655d1fbe49d4b089
Darren Duncan <darren@DarrenDuncan.net>
2023-02-04 18:29:02 -0800

    add wrapper Unix shell scripts muonp.sh and muldisder.sh for easier apps execution

---
+++ muonp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This wrapper script is very simple right now and assumes that when it is
+# invoked the current working directory is Muldis_Reference_Data_Tools_DotNet
+# checkout base dir; this will be improved.
+
+./Muldis_Object_Notation_Processor_Reference/Muldis.Object_Notation_Processor_Reference_App/bin/Debug/net6.0/Muldis.Object_Notation_Processor_Reference_App "$@"
