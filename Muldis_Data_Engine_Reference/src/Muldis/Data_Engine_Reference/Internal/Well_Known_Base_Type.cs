50b8d72982e6ce5514cfb45d735b505536d256c9
Darren Duncan <darren@DarrenDuncan.net>
2023-11-19 00:57:03 -0800

    Engine - move enum Well_Known_Base_Type from Value.cs to its own file

---
+++ Muldis_Data_Engine_Reference/src/Muldis/Data_Engine_Reference/Internal/Well_Known_Base_Type.cs
@@ -0,0 +1,32 @@
+namespace Muldis.Data_Engine_Reference.Internal;
+
+// Muldis.Data_Engine_Reference.Internal.Well_Known_Base_Type
+// Enumerates Muldis Data Language base types that are considered well-known to
+// this Muldis Data Language implementation, and that in particular have
+// their own dedicated handling code or formats in the implementation.
+// Every one of these types is either disjoint from or a proper subtype
+// or proper supertype of each of the others of these types, and in
+// particular many of these are proper subtypes of Article; however,
+// every Muldis Data Language value is considered to have a best fit into exactly
+// one of these types and that value is expected to be normalized
+// towards its best fit storage format.
+
+internal enum Well_Known_Base_Type
+{
+    MDL_Boolean,
+    MDL_Integer,
+    MDL_Fraction,
+    MDL_Bits,
+    MDL_Blob,
+    MDL_Text,
+    MDL_Array,
+    MDL_Set,
+    MDL_Bag,
+    MDL_Tuple,
+    MDL_Article,
+    MDL_Variable,
+    MDL_Process,
+    MDL_Stream,
+    MDL_External,
+    MDL_Excuse,
+}
