7c8c00da168a1f1eef5cdf8fb404c1f79899547b
Darren Duncan <darren@DarrenDuncan.net>
2023-11-18 23:29:36 -0800

    Engine - part 1 of split classes of MuldisServiceProtocolServer.cs into their own files, rename MuseMachine to MDER_Machine and MuseValue to MDER_V_Any

--- Muldis_Data_Engine_Reference/src/Muldis/Data_Engine_Reference/MDER_Machine.cs
+++ Muldis_Data_Engine_Reference/src/Muldis/Data_Engine_Reference/MDER_Machine.cs
@@ -706,32 +706,3 @@ public class MuseMachine
         }
     }
 }
-
-public class MuseValue
-{
-    internal MuseMachine m_machine;
-    internal Core.MD_Any m_value;
-
-    internal MuseValue init(MuseMachine machine)
-    {
-        m_machine = machine;
-        return this;
-    }
-
-    internal MuseValue init(MuseMachine machine, Core.MD_Any value)
-    {
-        m_machine = machine;
-        m_value   = value;
-        return this;
-    }
-
-    public override String ToString()
-    {
-        return m_value.ToString();
-    }
-
-    public MuseMachine MuseMachine()
-    {
-        return m_machine;
-    }
-}
