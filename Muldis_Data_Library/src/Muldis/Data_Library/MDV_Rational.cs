893df758c07c35036b42116253a461301b3d720a
Darren Duncan <darren@DarrenDuncan.net>
2024-01-19 23:18:03 -0800

    Library - make MDV_Rational a class rather than a struct since its fields might normalize lazily

--- Muldis_Data_Library/src/Muldis/Data_Library/MDV_Rational.cs
+++ Muldis_Data_Library/src/Muldis/Data_Library/MDV_Rational.cs
@@ -2,7 +2,7 @@ using System.Numerics;
 
 namespace Muldis.Data_Library;
 
-public struct MDV_Rational : MDV_Any, MDV_Fractional
+public sealed class MDV_Rational : MDV_Any, MDV_Fractional
 {
     private static readonly MDV_Rational __negative_one
         = new MDV_Rational(-1,1,true);
